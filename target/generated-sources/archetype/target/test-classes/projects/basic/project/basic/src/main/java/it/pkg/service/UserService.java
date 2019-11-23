c9bcd95c6d19958635a2959cd99479451e5563fb
feifeizai <xhf-office@foxmail.com>
2019-11-23 22:25:18 +0800

    '修改依赖版本'

--- target/generated-sources/archetype/target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package it.pkg.service;
-
-import it.pkg.entity.User;
-import org.springframework.transaction.annotation.Transactional;
-
-public interface UserService extends BaseService<User,Integer>{
-
-    User xxx(Integer id);
-}
